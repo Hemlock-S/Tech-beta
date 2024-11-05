32776f84f5e6423136da790c6029dea7107846ca
Siam Bhuiyan <bhuiyansiam077@gmail.com>
2024-11-05 21:26:57 +0600

    chore: slider component added

---
+++ src/components/Slider.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/1787236/pexels-photo-1787236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Close-Up Photo of Dslr Camera",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/1444416/pexels-photo-1444416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Turned-on Flat Screen Smart Television Ahead",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Top View Photo of Black Wireless Headphones",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/27424788/pexels-photo-27424788/free-photo-of-gamepad.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Gamepad",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Rose Gold Aluminum Case Apple Watch With White Sports Band",
+  },
+];
+
+const Slider = () => {
+  return (
+    <div className="slider">
+      {data.map((image) => (
+        <div
+          className="slide"
+          key={image.id}
+          style={{ backgroundImage: `url(${image.src})`}}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
